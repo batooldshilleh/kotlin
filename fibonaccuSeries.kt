a69dc17185b0a0ed90c8650c6b3a0887f3043a1b
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-02-14 15:23:39 +0200

    Coding conventions
    Indentation﻿ and whitespace

--- fibonaccuSeries.kt
+++ fibonaccuSeries.kt
@@ -4,11 +4,11 @@ fun main() {
     
 }
 
-fun fibonacci(number: Int){
+fun fibonacci(number: Int) {
     var term1 = 0 
     var term2 = 1
     println("the fibonaccu series for $number is :")
-    for(element in 1..number){
+    for(element in 1..number) {
         println(term1)
         val nextTerm = term1 + term2
         term1 = term2 
