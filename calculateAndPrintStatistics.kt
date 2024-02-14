e6e76c9e9a88dc48ed258fe250168f737010a392
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-02-14 15:02:17 +0200

    Coding conventions
    Indentation﻿ and whitespace

--- calculateAndPrintStatistics.kt
+++ calculateAndPrintStatistics.kt
@@ -1,30 +1,29 @@
-fun main() {
-    
-    var numbers = arrayOf(2,6,11,-10,-5,29,1,0,9)
+fun main() {  
+   var numbers = arrayOf(2,6,11,-10,-5,29,1,0,9)
    calculateAndPrintStatistics(numbers)
 }
 
 
-fun calculateAndPrintStatistics(numbers: Array<Int>){
-     var sum = 0
+fun calculateAndPrintStatistics(numbers: Array<Int>) {
+    var sum = 0
     var count = 0
     var product = 1
     val divisorForAverage = 1
     
-    for(number in numbers){
-        if(number > 0){
-          if(number >= 10){
-            sum += number
-            count++
+    for(number in numbers) {
+        if(number > 0) {
+	    if(number >= 10) {
+	        sum += number
+	        count++
         }
         
-        if( number% 3 == 0 ){
+        if(number% 3 == 0) {
             product *= number
         }
-        }
+      }
     }
     
-    if(count != 0){
+    if(count != 0) {
         val average = sum / count
         println("The average = $average")
     }
