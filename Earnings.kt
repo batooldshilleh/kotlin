a30fed43282bbb51fbadbd5816932268440d5bbb
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-02-14 14:50:19 +0200

    Coding conventions
    Horizontal whitespace & Indentation﻿

--- Earnings.kt
+++ Earnings.kt
@@ -1,16 +1,16 @@
-fun main() {
-    printEarnings(10.0,20.0)
-    printEarnings(55.2,25.8)
-    printEarnings(12.88,62.5)
+fun main()  {
+    printEarnings(10.0, 20.0)
+    printEarnings(55.2, 25.8)
+    printEarnings(12.88, 62.5)
 }
 
-fun printEarnings(sold: Double, salaryy: Double){
+fun printEarnings(sold: Double, salaryy: Double) {
     var salary = salaryy
     if (sold < 10.0)
-       salary = salary * 0.10
+    	salary = salary * 0.10
     else if (sold >=10.0 && sold <= 50.0)
-       salary = salary * 0.20
+    	salary = salary * 0.20
     else if (sold > 50.0)
-       salary = salary * 0.30
+    	salary = salary * 0.30
     println("Earnings: $salary")
 }
